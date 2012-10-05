422de282bde4536213f3388255f2011754a21fba
Takashiro <takashiro@qq.com>
2012-10-05 23:51:41 +0800

    更正软键盘目录名
    Signed-off-by: Takashiro <takashiro@qq.com>

--- OnscreenKeyboard/StdAfx.cpp
+++ OnscreenKeyboard/StdAfx.cpp
@@ -2,5 +2,5 @@
 //	OnscreenKeyboard.pch will be the pre-compiled header
 //	stdafx.obj will contain the pre-compiled type information
 // Download by http://www.codefans.net
-#include "stdafx.h"
 
+#include "stdafx.h"
